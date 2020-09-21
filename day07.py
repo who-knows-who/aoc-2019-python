6d5bd07ad0ce4e53afe4ec1b0c69e9a31de2d176
who-knows-who <j.williams97@outlook.com>
2020-09-21 20:53:44 +0100

    Tidy up day 7

--- day07.py
+++ day07.py
@@ -1,31 +1,63 @@
 from util import get_input, print_answer
-from intcode import make_program, execute_until_input
+from intcode import Program
 from itertools import permutations
-from collections import deque
+from queue import SimpleQueue
 
 
-def get_signal(order):
+def get_signal(program, phase_setting_sequence):
+    """
+    Return final output of series of program copies initiated with specified phase settings
+
+    Parameters:
+    program (Program object): the intcode program for day 7
+    phase_setting_sequence (int[]): list of integer phase settings
+
+    Returns:
+    int: output of final machine
+    """
+
+    queue = SimpleQueue()
+
+    # First run should consider [0] as output buffer
     output_buffer = [0]
-    waiting = deque()
-    for setting in order:
-        waiting.append({"program_state": (program.copy(), 0, 0), "input_buffer": [setting]})
-    while waiting:
-        amp = waiting.popleft()
-        amp["input_buffer"].extend(output_buffer)
-        exit_code, output_buffer, program_state = execute_until_input(amp["program_state"], amp["input_buffer"])
-        if exit_code == 1:
-            waiting.append({"program_state": program_state, "input_buffer": []})
+
+    for phase_setting in phase_setting_sequence:
+
+        # Create copy of program and run with phase setting as input
+        program_copy = program.copy()
+        program_copy.run([phase_setting])
+
+        # Add paused program to queue
+        queue.put(program_copy)
+
+    # Loop until all programs finish/halt
+    while not queue.empty():
+
+        # Rerun programs with output buffer as next input
+        program = queue.get()
+        program.run(output_buffer)
+
+        # Get output buffer on halt/pause
+        output_buffer = program.output_buffer
+
+        # If program has paused, re-add to queue
+        if not program.finished:
+            queue.put((program))
+
+    # Return final output (should only be 1 element in list)
     return output_buffer[0]
 
 
-def part1():
-    signal_orders = list(permutations(range(0, 5)))
-    return max([get_signal(order) for order in signal_orders])
+def part1(program):
+
+    # Find maximum output with all permutations of inputs 0-5
+    return max([get_signal(program, order) for order in permutations(range(0, 5))])
 
 
-def part2():
-    signal_orders = list(permutations(range(5, 10)))
-    return max([get_signal(order) for order in signal_orders])
+def part2(program):
+
+    # Find maximum output with all permutations of inputs 5-9
+    return max([get_signal(program, order) for order in permutations(range(5, 10))])
 
 
 if __name__ == "__main__":
@@ -33,7 +65,7 @@ if __name__ == "__main__":
     part1_correct = 95757
     part2_correct = 4275738
 
-    program = make_program(get_input("07", split=True))
+    program = Program(7)
 
-    print_answer(1, part1(), part1_correct)
-    print_answer(2, part2(), part2_correct)
+    print_answer(1, part1(program), part1_correct)
+    print_answer(2, part2(program), part2_correct)
