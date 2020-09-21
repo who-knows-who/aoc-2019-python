724cd0d96f45994a8e2462881bb87ca2974e3b42
who-knows-who <j.williams97@outlook.com>
2020-09-21 16:43:54 +0100

    Day 20 part 2 working

--- day20.py
+++ day20.py
@@ -2,92 +2,184 @@ from util import get_input, print_answer, get_orthogonal_neighbours
 from heapq import heappop, heappush
 
 
-def part1(portal_names, portal_locations, walkable):
+def find_portals(maze):
+    """
+    Return details of portals in maze.
 
-    seen = set()
+    Parameters:
+    maze (str[]): list of strings representing the maze.
+
+    Returns:
+    portal_names (dict of (int, int): string): dictionary mapping coordinates (x, y) to portal names.
+    portal_locations (dict of string: (int, int){}): dictionary mapping portal names to sets of coordinates (x, y).
+    portal_sides (dict of (int, int): string): dictionary mapping coordinates (x, y) to "inner" or "outer" wall of maze.
+    """
+
+    portal_names = dict()
+    portal_locations = dict()
+    portal_sides = dict()
+
+    height = len(maze)
+    width = len(maze[0])
+
+    for y, line in enumerate(maze):
+        for x, char in enumerate(line):
+            # Upper case letters indicate portals
+            if char.isupper():
+                if y < height - 2 and maze[y+1][x].isupper() and maze[y+2][x] == '.':
+                    # X <- char
+                    # X
+                    # .
+                    name = char + maze[y+1][x]
+                    location = (x, y+2)
+                    if y < height / 2:
+                        side = "outer"
+                    else:
+                        side = "inner"
+                    store_portal(portal_names, portal_locations, portal_sides,
+                                 name, location, side)
+                if y < height - 1 and maze[y+1][x].isupper() and maze[y-1][x] == '.':
+                    # .
+                    # X <- char
+                    # X
+                    name = char + maze[y+1][x]
+                    location = (x, y-1)
+                    if y < height / 2:
+                        side = "inner"
+                    else:
+                        side = "outer"
+                    store_portal(portal_names, portal_locations, portal_sides,
+                                 name, location, side)
+                if x < width - 2 and maze[y][x+1].isupper() and maze[y][x+2] == '.':
+                    # XX.
+                    # ^
+                    name = char + maze[y][x+1]
+                    location = (x+2, y)
+                    if x < width / 2:
+                        side = "outer"
+                    else:
+                        side = "inner"
+                    store_portal(portal_names, portal_locations, portal_sides,
+                                 name, location, side)
+                if x < width - 1 and maze[y][x+1].isupper() and maze[y][x-1] == '.':
+                    # .XX
+                    #  ^
+                    name = char + maze[y][x+1]
+                    location = (x-1, y)
+                    if x < width / 2:
+                        side = "inner"
+                    else:
+                        side = "outer"
+                    store_portal(portal_names, portal_locations, portal_sides,
+                                 name, location, side)
+    return portal_names, portal_locations, portal_sides
+
+
+def store_portal(portal_names, portal_locations, portal_sides, name, location, side):
+
+    portal_names[location] = name
+    portal_sides[location] = side
+    if name in portal_locations:
+        # Other portal with name already found - add to existing set
+        portal_locations[name].add(location)
+    else:
+        # First portal with name found - make new entry in dict
+        portal_locations[name] = {location}
+
+
+def get_walkable(maze):
+    """
+    Return set of coordinates (x, y) for walkable positions in maze.
+
+    Parameters:
+    maze (str[]): list of strings representing the maze.
+    """
+    return {(x, y) for y, line in enumerate(maze) for x, char in enumerate(line) if line[x] == '.'}
+
+
+def get_shortest_route(portal_names, portal_locations, walkable, portal_sides=dict()):
+    """
+    Return length of shortest route from AA to ZZ.
+
+    Parameters:
+    portal_names (dict of (int, int): string): dictionary mapping coordinates (x, y) to portal names.
+    portal_locations (dict of string: (int, int){}): dictionary mapping portal names to sets of coordinates (x, y).
+    walkable ((int, int){}): set of coordinates (x, y) of walkable positions
+    portal_sides (dict of (int, int): string): dictionary mapping coordinates (x, y) to "inner" or "outer" wall of maze. (Optional, default=False).
+
+    Returns:
+    int: length of shortest route
+    """
 
     # Start from maze entrace - AA
-    q = [(0, portal_locations['AA'])]
+    q = [(0, 0, start_location) for start_location in portal_locations['AA']]
+    seen = set()
 
     while q:
-        q_distance, q_pos = heappop(q)
+        q_distance, q_level, q_pos = heappop(q)
 
-        # queue is sorted by distance, first route to exit is shortest
-        if q_pos == portal_locations['ZZ']:
-            return q_distance
-        
-        # Maze is fixed so only need to consider a location once
-        if q_pos in seen:
+        # Maze is fixed so only need to consider a location/level pair once
+        if (q_pos, q_level) in seen:
             continue
-        seen.add(q_pos)
+        seen.add((q_pos, q_level))
 
+        # If landed on a portal
         if q_pos in portal_names:
             portal_name = portal_names[q_pos]
             if portal_name not in ['AA', 'ZZ']:
-                for portal in (portal_locations[portal_name] - {q_pos}):
-                    if portal not in seen:
-                        heappush(q, (q_distance + 1, portal))
 
+                # Get new level when moving through portal
+                if not portal_sides:
+                    # If not using portal sides (part1) level always 0
+                    n_level = 0
+                elif portal_sides[q_pos] == "outer":
+                    # If portal is outer side than level decreases
+                    n_level = q_level - 1
+                    print("Recurse up through", portal_name, "to", n_level)
+                elif portal_sides[q_pos] == "inner":
+                    # If portal in inner side than level increases
+                    n_level = q_level + 1
+                    print("Recurse down through", portal_name, "to", n_level)
+                else:
+                    print("Unknown portal side", portal_sides[q_pos])
+                    exit()
+
+                # Level cannot be less than 0, outer portals act as walls in level 0
+                if n_level >= 0:
+                    # Find opposite portal and add to queue
+                    for portal in (portal_locations[portal_name] - {q_pos}):
+                        if (portal, n_level) not in seen:
+                            heappush(q, (q_distance + 1, n_level, portal))
+
+        # Add walkable neighbours to queue
         for position in get_orthogonal_neighbours(q_pos):
+            if position in portal_locations['ZZ'] and q_level == 0:
+                # queue is sorted by distance, first route to exit is shortest
+                return q_distance + 1
             if position in walkable - seen:
-                heappush(q, (q_distance + 1, position))     
-
-def part2():
-    return 0
+                heappush(q, (q_distance + 1, q_level, position))
 
 
-def find_portals(maze):
-    portal_names = dict()
-    portal_locations = dict()
-    height = len(maze)
-    width = len(maze[0])
-    for y, line in enumerate(maze):
-        for x, char in enumerate(line):
-            if char.isupper():
-                if y < height - 2 and maze[y+1][x].isupper() and maze[y+2][x] == '.':
-                    name = char + maze[y+1][x]
-                    location = (x, y+2)
-                    store_portal(portal_names, portal_locations,
-                                 name, location)
-                if y < height - 1 and maze[y+1][x].isupper() and maze[y-1][x] == '.':
-                    name = char + maze[y+1][x]
-                    location = (x, y-1)
-                    store_portal(portal_names, portal_locations,
-                                 name, location)
-                if x < width - 2 and maze[y][x+1].isupper() and maze[y][x+2] == '.':
-                    name = char + maze[y][x+1]
-                    location = (x+2, y)
-                    store_portal(portal_names, portal_locations,
-                                 name, location)
-                if x < width - 1 and maze[y][x+1].isupper() and maze[y][x-1] == '.':
-                    name = char + maze[y][x+1]
-                    location = (x-1, y)
-                    store_portal(portal_names, portal_locations,
-                                 name, location)
-    return portal_names, portal_locations
+def part1(portal_names, portal_locations, walkable):
+
+    return get_shortest_route(portal_names, portal_locations, walkable)
 
 
-def store_portal(portal_names, portal_locations, name, location):
-    portal_names[location] = name
-    if name in ['AA', 'ZZ']:
-        portal_locations[name] = location
-    elif name in portal_locations:
-        portal_locations[name].add(location)
-    else:
-        portal_locations[name] = {location}
+def part2(portal_names, portal_locations, walkable, portal_sides):
 
-def get_walkable(maze):
-    return {(x, y) for y, line in enumerate(maze) for x, char in enumerate(line) if line[x] == '.'}
+    return get_shortest_route(portal_names, portal_locations, walkable, portal_sides)
 
 
 if __name__ == "__main__":
 
     part1_correct = 596
-    part2_correct = None
+    part2_correct = 7610
 
     maze = get_input(20, strip=False)
-    portal_names, portal_locations = find_portals(maze)
+    portal_names, portal_locations, portal_sides = find_portals(maze)
     walkable = get_walkable(maze)
 
-    print_answer(1, part1(portal_names, portal_locations, walkable), part1_correct)
-    print_answer(2, part2(), part2_correct)
+    print_answer(1, part1(portal_names, portal_locations,
+                          walkable), part1_correct)
+    print_answer(2, part2(portal_names, portal_locations,
+                          walkable, portal_sides), part2_correct)
--- day20_test.py
+++ day20_test.py
@@ -1,6 +1,6 @@
 import unittest
 from util import get_input
-from day20 import find_portals, part1, part2
+from day20 import find_portals, get_walkable, part1, part2
 
 
 class ClientTest(unittest.TestCase):
@@ -9,11 +9,14 @@ class ClientTest(unittest.TestCase):
         maze = get_input(20, test=1, strip=False)
         expected_names = {(9, 2): 'AA', (9, 6): 'BC', (2, 8): 'BC',
                           (6, 10): 'DE', (2, 13): 'DE', (11, 12): 'FG', (2, 15): 'FG', (13, 16): 'ZZ'}
-        expected_locations = {'AA': (9, 2), 'BC': {(9, 6), (2, 8)},
-                              'DE': {(6, 10), (2, 13)}, 'FG': {(11, 12), (2, 15)}, 'ZZ': (13, 16)}
-        portal_names, portal_locations = find_portals(maze)
+        expected_locations = {'AA': {(9, 2)}, 'BC': {(9, 6), (2, 8)},
+                              'DE': {(6, 10), (2, 13)}, 'FG': {(11, 12), (2, 15)}, 'ZZ': {(13, 16)}}
+        expected_sides = {(9, 2): 'outer', (9, 6): 'inner', (2, 8): 'outer',
+                          (6, 10): 'inner', (2, 13): 'outer', (11, 12): 'inner', (2, 15): 'outer', (13, 16): 'outer'}
+        portal_names, portal_locations, portal_sides = find_portals(maze)
         self.assertEqual(portal_names, expected_names)
         self.assertEqual(portal_locations, expected_locations)
+        self.assertEqual(portal_sides, expected_sides)
 
     def test_walkable(self):
         maze = get_input(20, test=1, strip=False)
@@ -23,16 +26,20 @@ class ClientTest(unittest.TestCase):
                              (17, 15), (16, 15), (15, 15), (14, 15), (13, 15), (13, 16), (13, 14), (13, 13), 
                              (12, 13), (11, 13), (11, 12), (2, 15), (3, 15), (3, 14), (3, 13), (2, 13), 
                              (2, 8), (3, 8), (4, 8), (4, 9), (4, 10), (5, 10), (6, 10)}
-        walkable = {(x, y) for y, line in enumerate(maze)
-                    for x, char in enumerate(line) if line[x] == '.'}
+        walkable = get_walkable(maze)
         self.assertEqual(walkable, expected_walkable)
 
-    def test_example1(self):
+    def test_part1(self):
         maze = get_input(20, test=1, strip=False)
-        portal_names, portal_locations = find_portals(maze)
-        walkable = {(x, y) for y, line in enumerate(maze)
-                    for x, char in enumerate(line) if line[x] == '.'}
+        portal_names, portal_locations, _ = find_portals(maze)
+        walkable = get_walkable(maze)
         self.assertEqual(part1(portal_names, portal_locations, walkable), 23)
+
+    # def test_part2(self):
+    #     maze = get_input(20, test=2, strip=False)
+    #     portal_names, portal_locations, portal_sides = find_portals(maze)
+    #     walkable = get_walkable(maze)
+    #     self.assertEqual(part2(portal_names, portal_locations, walkable, portal_sides), 396)
 
 
 if __name__ == '__main__':
